0ceae7b1836ae16ad218f6ff417e34987100d93b
Patrick Nance <jpnance@gmail.com>
2022-11-22 15:06:47 -0800

    Adding a bunch of sort stuff.

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,13 +1,26 @@
 import SailingCard, { Sailing } from './SailingCard'
 
 type SearchResultsProps = {
-	results: Sailing[];
+	filteredResults: Sailing[];
+	totalResults: number;
+	sortBy: (sortMethodString: string) => void;
 };
 
-const SearchResults = ({ results }: SearchResultsProps) => {
+const SearchResults = ({ filteredResults, totalResults, sortBy }: SearchResultsProps) => {
 	return (
 		<div>
-			{results.map((result, i) => <SailingCard key={`result-${i}`} sailing={result} /> )}
+			<div className="flex flex-col md:flex-row items-center">
+				<div className="font-bold grow">{totalResults} trips found</div>
+				<select className="rounded-md p-2" onChange={(event) => sortBy(event.target.value)}>
+					<option value="price-asc">Price: Lowest first</option>
+					<option value="price-desc">Price: Highest first</option>
+					<option value="departure-asc">Departure date: Earliest first</option>
+					<option value="departure-desc">Departure date: Latest first</option>
+					<option value="duration-asc">Duration: Shortest first</option>
+					<option value="duration-desc">Duration: Longest first</option>
+				</select>
+			</div>
+			{filteredResults.map((result, i) => <SailingCard key={`result-${i}`} sailing={result} /> )}
 		</div>
 	)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,12 +11,16 @@ const Home: NextPage = () => {
 	const [allResults, setAllResults] = useState([])
 	const [filteredResults, setFilteredResults] = useState([] as Sailing[])
 	const [pageIndex, setPageIndex] = useState(0)
+	const [sortMethod, setSortMethod] = useState({ field: 'price', ascending: true })
 
 	useEffect(() => {
 		fetch('https://sandbox.cruisebound-qa.com/sailings')
 			.then((response) => response.json())
 			.then((response) => {
-				setAllResults(response.results.sort((a: Sailing, b: Sailing) => a.price - b.price))
+				let results = response.results
+				results.sort(resultsSort)
+
+				setAllResults(response.results)
 				setFilteredResults(response.results)
 			})
 
@@ -32,13 +36,60 @@ const Home: NextPage = () => {
 		}
 	}
 
+	let resultsSort = (a: Sailing, b: Sailing) => {
+		if (sortMethod.field === 'price') {
+			if (sortMethod.ascending) {
+				return a.price - b.price
+			}
+			else {
+				return b.price - a.price
+			}
+		}
+		else if (sortMethod.field === 'departure') {
+			if (sortMethod.ascending) {
+				return a.departureDate < b.departureDate ? 1 : -1
+			}
+			else {
+				return a.departureDate > b.departureDate ? 1 : -1
+			}
+		}
+		else if (sortMethod.field === 'duration') {
+			if (sortMethod.ascending) {
+				return a.duration - b.duration
+			}
+			else {
+				return b.duration - a.duration
+			}
+		}
+
+		return 0
+	}
+
+	let sortBy = (sortMethodString: string) => {
+		let [field, ascending] = sortMethodString.split('-')
+
+		setSortMethod({ field: field, ascending: ascending === 'asc' })
+	}
+
+	useEffect(() => {
+		let newFilteredResults = filteredResults.slice()
+
+		newFilteredResults.sort(resultsSort)
+		setFilteredResults(newFilteredResults)
+
+	}, [sortMethod])
+
 	return (
 		<div className="flex flex-col lg:flex-row min-h-screen justify-center">
 			<div className="p-4 mx-2 my-2 bg-slate-800 text-white">
 				<Filters allResults={allResults} setFilteredResults={setFilteredResults} resetPagination={changePageIndex.bind(null, 0)} />
 			</div>
 			<div className="flex m-2 flex-col items-start justify-center max-w-4xl">
-				<SearchResults results={filteredResults.slice(pageIndex * resultsPerPage, pageIndex * resultsPerPage + resultsPerPage)} />
+				<SearchResults
+					filteredResults={filteredResults.slice(pageIndex * resultsPerPage, pageIndex * resultsPerPage + resultsPerPage)}
+					totalResults={filteredResults.length}
+					sortBy={sortBy}
+				/>
 				<Pagination totalPages={Math.ceil(filteredResults.length / resultsPerPage)} currentPageIndex={pageIndex} changePageIndex={changePageIndex} />
 			</div>
 		</div>
