b270397a192ef59b62917ae907a7823a3ca2d194
Patrick Nance <jpnance@gmail.com>
2022-11-21 22:13:48 -0800

    Making the pagination widget work more like the comp shows.

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -5,21 +5,38 @@ type PaginationProps = {
 }
 
 const Pagination = ({ totalPages, currentPageIndex, changePageIndex }: PaginationProps) => {
-	let pageIndexes = [];
+	const paginationBubble = 2;
+
+	let pages = [];
+	let ellipses = false;
 
 	for (let i = 0; i < totalPages; i++) {
-		pageIndexes.push(i + 1);
+		if (i === 0 || i === totalPages - 1 || (i >= currentPageIndex - paginationBubble && i <= currentPageIndex + paginationBubble)) {
+			pages.push({ index: i, number: i + 1 });
+			ellipses = false;
+		}
+		else if (!ellipses) {
+			pages.push(null);
+			ellipses = true;
+		}
 	}
 
 	return (
-		<div className="flex p-2 bg-slate-100 align-items-start rounded-md">
+		<div className="text-xs md:text-md flex p-2 bg-slate-100 align-items-start rounded-md">
 			<div className={`text-center p-2 cursor-pointer font-bold ${currentPageIndex === 0 ? 'text-gray-200' : 'text-blue-500'}`} onClick={(event) => changePageIndex(currentPageIndex - 1)}>❮</div>
-			{pageIndexes.map((pageNumber, i) => {
-				return (
-					<div className={`text-center p-2 cursor-pointer font-bold ${i == currentPageIndex ? 'bg-white rounded-full' : ''}`} key={`pagination-${i}`} onClick={(event) => changePageIndex(i)}>
-						{pageNumber}
-					</div>
-				)
+			{pages.map((page) => {
+				if (page !== null) {
+					return (
+						<div className={`text-center p-2 cursor-pointer font-bold ${page.index == currentPageIndex ? 'bg-white rounded-full' : ''}`} key={`pagination-${page.index}`} onClick={(event) => changePageIndex(page.index)}>
+							{page.number}
+						</div>
+					)
+				}
+				else {
+					return (
+						<div className="text-center p-2 font-bold">...</div>
+					)
+				}
 			})}
 			<div className={`text-center p-2 cursor-pointer font-bold ${currentPageIndex === totalPages - 1 ? 'text-gray-200' : 'text-blue-500'}`} onClick={(event) => changePageIndex(currentPageIndex + 1)}>❯</div>
 		</div>
